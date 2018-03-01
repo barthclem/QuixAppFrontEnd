d3703591b956a7a451fec795f52575ceb7000e50
barthclem <barthclem@gmail.com>
2018-03-01 07:30:05 +0100

    updated production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ws_url: 'http://localhost:5000',
-  ws_url_secure: 'https://localhost:3300'
+  ws_url_secure: 'https:// 139.162.198.97',
+  api_url_secure: 'https:// 139.162.198.97:4000'
 };
